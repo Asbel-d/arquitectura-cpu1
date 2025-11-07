cdf14db192f0874db8357c89b1e98a661cb10f90
Asbel Enrique Duque Vanegas <asbel.duque@utp.edu.co>
2025-11-06 20:26:31 -0500

    Refactor data_memory module input/output declarations

--- data_memory.sv
+++ data_memory.sv
@@ -1,18 +1,14 @@
-// ========================================================
-// DATA MEMORY - RISC-V (para instrucciones tipo I y S)
-// Compatible con DE1-SoC y direccionamiento por bytes
-// ========================================================
 module data_memory (
     input  logic        clk,
-    input  logic        mem_write,   // Habilita escritura (store)
-    input  logic        mem_read,    // Habilita lectura (load)
-    input  logic [31:0] addr,        // Dirección byte a byte
-    input  logic [31:0] write_data,  // Datos a escribir (para store)
-    output logic [31:0] read_data    // Datos leídos (para load)
+    input  logic        mem_write,
+    input  logic        mem_read,
+    input  logic [31:0] addr,
+    input  logic [31:0] write_data,
+    output logic [31:0] read_data
 );
 
-    // Memoria de 1 KB = 256 palabras de 32 bits
-    logic [7:0] mem [0:1023]; // 1024 bytes (byte-addressable)
+    
+    logic [7:0] mem [0:1023];
 
     integer i;
 
@@ -22,14 +18,14 @@ module data_memory (
             mem[i] = 8'h00;
         end
 
-        // Algunos datos de prueba:
+    
         mem[0]  = 8'hAA;
         mem[1]  = 8'hBB;
         mem[2]  = 8'hCC;
         mem[3]  = 8'hDD;
     end
 
-    // Escritura síncrona (solo si mem_write = 1)
+    
     always_ff @(posedge clk) begin
         if (mem_write) begin
             mem[addr]     <= write_data[7:0];
@@ -39,7 +35,7 @@ module data_memory (
         end
     end
 
-    // Lectura combinacional (solo si mem_read = 1)
+    
     always_comb begin
         if (mem_read) begin
             read_data = { mem[addr + 3],
